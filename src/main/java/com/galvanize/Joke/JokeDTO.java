cc977bb5017d6d627cedf396b897f8645a456307
Wes Hediger <wes.hediger@galvanize.com>
2021-06-14 15:23:33 -0400

    add jokeDTO

---
+++ src/main/java/com/galvanize/Joke/JokeDTO.java
@@ -0,0 +1,13 @@
+package com.galvanize.Joke;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class JokeDTO {
+
+    String joke;
+}
