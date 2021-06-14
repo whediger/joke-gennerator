35dc092085da66206d087c96017c708e14897ec0
Wes Hediger <wes.hediger@galvanize.com>
2021-06-14 15:43:28 -0400

    RED update test

--- src/test/java/com/galvanize/Joke/JokeTest.java
+++ src/test/java/com/galvanize/Joke/JokeTest.java
@@ -3,19 +3,23 @@ package com.galvanize.Joke;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.beans.factory.annotation.Autowired;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
+@ExtendWith(MockitoExtension.class)
 public class JokeTest {
 
-    @Autowired
-    JokeService jokeService;
+    @InjectMocks
+    JokeService subject;
 
     @Test
     @DisplayName("get newly generated joke from third party API")
     void getNewJoke() {
-        JokeDTO newJoke = jokeService.generateJoke();
-        assertThat(newJoke.joke).isEqualTo("new joke");
+        JokeDTO actual = subject.generateJoke();
+        assertThat(actual.getJoke()).isEqualTo("new joke");
     }
 }
